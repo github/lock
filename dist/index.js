a11dd7af90f196bfcf940d7807a54376fce6e396
grantbirki <grantbirki@github.com>
2023-03-13 14:32:21 +0000

    bundle

--- dist/index.js
+++ dist/index.js
@@ -10449,8 +10449,14 @@ async function unlock(
 
 
 
+// Helper function to check if a lock branch + lock file exists
+// :param octokit: The authenticated octokit instance
+// :param context: The github context
+// :param branch: The branch to check for a lock file
+// :returns: True if the lock file exists, false otherwise
 async function checkLockFile(octokit, context, branch) {
   try {
+    // try to get the lock branch
     await octokit.rest.repos.getBranch({
       ...context.repo,
       branch: branch
